91a901aab07ef1143ef896a56d45e4bc6e7cfcf5
CogentInvalid <pchipman@email.arizona.edu>
2017-10-28 12:02:54 -0700

    Potion display

--- game/gameUI.lua
+++ game/gameUI.lua
@@ -4,18 +4,21 @@ function gameUI:initialize(parent)
 	self.parent = parent
 	self.player = nil
 	self.hpDisplay = 0
+	self.display = "Current potion: none"
 end
 
 function gameUI:update(dt)
 	self.player = self.parent.player
 	if self.player ~= nil then
-		self.hpDisplay = self.player.health.hp
+		local potionThrower = self.player.potions
+		local currentPotion = potionThrower.potions[potionThrower.potionIndex]
+		self.display = "Current potion: " .. currentPotion.name
 	end
 end
 
 function gameUI:draw()
 	love.graphics.setColor(255, 255, 255)
-	love.graphics.print("wip ui " .. self.hpDisplay, 10, 10)
+	love.graphics.print(self.display, 10, 10)
 end
 
 return gameUI

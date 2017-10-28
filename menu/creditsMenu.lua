72ff76b4e717c80c27c435f508abde7604c5d29a
BuildTools <carlwernicke86@gmail.com>
2017-10-28 16:12:33 -0700

    creidts updated

--- menu/creditsMenu.lua
+++ menu/creditsMenu.lua
@@ -24,6 +24,17 @@ function creditsMenu:draw()
 	love.graphics.setNewFont("/res/font/CourierCode-bold.ttf", 100)
 	local w = love.graphics.getFont():getWidth("DEVELOPERS")
 	love.graphics.print("DEVELOPERS", love.graphics.getWidth()/2-w/2, 25)
+	love.graphics.setNewFont("/res/font/CourierCode-bold.ttf", 75)
+	
+	width = love.graphics.getFont():getWidth("CARL")
+	love.graphics.print("CARL", love.graphics.getWidth()/2 - width/2, 150)
+	width = love.graphics.getFont():getWidth("PETER")
+	love.graphics.print("PETER", love.graphics.getWidth()/2 - width/2, 225)
+	width = love.graphics.getFont():getWidth("TAYLOR")
+	love.graphics.print("TAYLOR", love.graphics.getWidth()/2 - width/2, 300)
+	width = love.graphics.getFont():getWidth("VICTOR")
+	love.graphics.print("VICTOR", love.graphics.getWidth()/2 - width/2, 375)
+	
 	
 	love.graphics.setNewFont("/res/font/CourierCode-bold.ttf", 24)
 	suit.draw()
